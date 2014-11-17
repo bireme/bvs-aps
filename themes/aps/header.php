988b747fd81d8aa6e11f31f4ec0a9667e36f8cf4
Moacir <moa.moda@gmail.com>
2014-11-17 12:56:40 -0200

    ajustando o title do header e criando páginas temas das sof

--- themes/aps/header.php
+++ themes/aps/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php wp_title('|', true, 'right'); ?></title>
+    <title><?php wp_title('BVS APS |'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" type="text/css" href="<?= get_stylesheet_directory_uri(); ?>/reset.css" />
