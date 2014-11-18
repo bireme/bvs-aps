dc449be806477fe0f2c064919a2ed3296370e0d4
Moacir <moa.moda@gmail.com>
2014-11-18 13:39:13 -0200

    ajustando archives

--- themes/aps/archive-aps.php
+++ themes/aps/archive-aps.php
@@ -36,9 +36,10 @@ get_header(); ?>
 							<img src="<?php echo z_taxonomy_image_url($term->term_id, 'single-thumb-square'); ?>" />
 						<?php break; endforeach; endif; ?>
 					</div>
+	
+					<h3><?php the_terms(get_the_ID(), 'area-tematica'); ?><?php the_terms(get_the_ID(), 'categoria-da-evidencia'); ?></h3>
 				<?php endif; ?>
 			
-				<h3><?php the_terms(get_the_ID(), 'area-tematica'); ?><?php the_terms(get_the_ID(), 'categoria-da-evidencia'); ?></h3>
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				<?= the_post_thumbnail('research-thumb'); ?>
 				<p><?php the_excerpt(); ?></p>
--- themes/aps/archive-pearl.php
+++ themes/aps/archive-pearl.php
@@ -19,14 +19,17 @@
 			
 			<div class="item">
 				
-				<div class="thumb">
-					<?php if(!empty($terms)): foreach($terms as $term): ?>
-						<img src="<?php echo z_taxonomy_image_url($term->term_id, 'single-thumb-square'); ?>" />
-					<?php break; endforeach; endif; ?>
-				</div>
+				<?php if(!is_tax()): ?>
+					<div class="thumb">
+						<?php if(!empty($terms)): foreach($terms as $term): ?>
+							<img src="<?php echo z_taxonomy_image_url($term->term_id, 'single-thumb-square'); ?>" />
+						<?php break; endforeach; endif; ?>
+					</div>
+	
+					<h3><?php the_terms(get_the_ID(), 'area-tematica'); ?><?php the_terms(get_the_ID(), 'categoria-da-evidencia'); ?></h3>
+				<?php endif; ?>
 			
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				<h3><?php the_terms('area-tematica'); ?><?php the_terms('categoria-da-evidencia'); ?></h3>
 				<?= the_post_thumbnail('research-thumb'); ?>
 				<p><?php the_excerpt(); ?></p>
 
