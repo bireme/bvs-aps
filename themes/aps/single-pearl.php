94a354b7fd3d1df3ab355f112cdc8620dbeb27b4
Moacir <moa.moda@gmail.com>
2014-11-18 18:23:16 -0200

    reajustando tamanho de thumb das pearls

--- themes/aps/single-pearl.php
+++ themes/aps/single-pearl.php
@@ -14,11 +14,11 @@
 				
 				<div class="thumb">
 					<?php foreach (get_the_terms(get_the_ID(), 'area-tematica') as $cat): ?>
-						<img src="<?php echo z_taxonomy_image_url($cat->term_id, 'single-thumb-square'); ?>" />
+						<img src="<?php echo z_taxonomy_image_url($cat->term_id, 'single-thumb'); ?>" />
 					<?php break; endforeach; ?>
 
 					<?php foreach (get_the_terms(get_the_ID(), 'categoria-da-evidencia') as $cat): ?>
-						<img src="<?php echo z_taxonomy_image_url($cat->term_id, 'single-thumb-square'); ?>" />
+						<img src="<?php echo z_taxonomy_image_url($cat->term_id, 'single-thumb'); ?>" />
 					<?php break; endforeach; ?>
 				</div>
 				
