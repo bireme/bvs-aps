e58c9ef6f5a71f28832c53a565d3486d41240901
Moacir <moa.moda@gmail.com>
2014-11-27 17:36:00 -0200

    removendo commit do juliao para nao haver conflito

--- themes/aps/functions.php
+++ themes/aps/functions.php
@@ -91,7 +91,6 @@ function ttruncat($text,$numb) {
     return $text; 
 }
 
-add_theme_support( 'post-thumbnails' );
 add_image_size( 'single-thumb', 500, 100, true ); // (cropped)
 add_image_size( 'single-thumb-square', 500, 300, true ); // (cropped)
 
